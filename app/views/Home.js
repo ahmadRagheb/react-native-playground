72fd5ecb024743fcd7fb7ff7dde05fec26c25586
ahmadragheb <ahmedragheb75@gmail.com>
2019-04-04 04:06:49 +0300

    first commit

---
+++ app/views/Home.js
@@ -0,0 +1,13 @@
+import  React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export class Home extends React.Component{
+  render(){
+    return(
+    <View>
+    <Text>This will be the Homepage</Text>
+    <Text>These other lines are here</Text>
+    <Text>So you can see the text in the app </Text>
+    </View>)
+  }
+}
